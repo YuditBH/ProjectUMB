663d82d0e948726db13d1b3b7f3678411e1fe3dc
YuditBH <juditBH73@gmail.com>
2022-10-01 21:46:33 +0300

    Add files via upload

---
+++ Helpers/QuerryStringHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UProject.Helpers
+{
+    public class QuerryStringHelper
+    {
+        public static int GetIntFromQueryString(HttpRequestBase request, string key, int fallbackValue = 0)
+        {
+            if (!string.IsNullOrWhiteSpace(key))
+            {
+                if (request.QueryString[key] != null && !string.IsNullOrWhiteSpace(request.QueryString[key]) && int.TryParse(request.QueryString[key].ToString(), out var numericValue))
+                {
+                    return numericValue;
+                }
+            }
+            return fallbackValue;
+        }
+    }
+}
