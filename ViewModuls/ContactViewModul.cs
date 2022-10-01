1461bb7cbfefc50bd5b4d88be98b0bff4f17412e
YuditBH <juditBH73@gmail.com>
2022-10-01 21:49:25 +0300

    Add files via upload

---
+++ ViewModuls/ContactViewModul.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+namespace UProject.ViewModuls
+{
+    public class ContactViewModul
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(500, ErrorMessage = "Your message must be 500 characters or less")]
+        public string Message { get; set; }
+
+        public int ContactFormId { get; set; }
+    }
+}
