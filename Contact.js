90fc37e757fe8c453e26e7ab6b00427ae50d2256
YuditBH <juditBH73@gmail.com>
2022-10-02 07:43:15 +0300

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,18 @@
+﻿var contactForm = contactForm ||
+{
+    unit: function () {
+        this.listeners();
+    },
+    listeners: function () {
+        $(document).on('click', '.contact-submit', function () {
+            e.preventDefaut();
+            var form = $("#contact-form");
+            form.submit();
+        });
+    },
+    showResult: function () {
+        $('#form-outer').hide('slow');
+        $('#form-result').show('slow');
+    }
+}
+
